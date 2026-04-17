b7bf6d83982ba18960d733bfdf32ef147fa452b4
KevinTimaran <kevin.buesaquillo@campusucc.edu.co>
2026-04-17 15:02:32 -0500

    v1

---
+++ microservices-monitoring/src/test/java/com/monitoring/microservicesmonitoring/MicroservicesMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.microservicesmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
